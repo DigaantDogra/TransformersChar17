6ed1f9d55c8b5e46b34507093adfbc7251d2a555
Digaant Singh Dogra <128942073+DigaantDogra@users.noreply.github.com>
2024-06-24 23:16:11 -0400

    Initial Commit

---
+++ TransformersChar17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransformersChar17
+//
+//  Created by Digaant Dogra on 2024-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransformersChar17/TransformersChar17App.swift
@@ -0,0 +1,17 @@
+//
+//  TransformersChar17App.swift
+//  TransformersChar17
+//
+//  Created by Digaant Dogra on 2024-06-24.
+//
+
+import SwiftUI
+
+@main
+struct TransformersChar17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
